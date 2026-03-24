1dc65248b7269d0342f28b1958940cd60633f7cb
Jonathan V Tran <me@jonathanvutran.com>
2026-03-24 17:12:49 -0500

    Remove Storybook and related dependencies
    - Delete .storybook/ config, src/stories/, and storybook deploy workflow
    - Remove @storybook/*, @chromatic-com/storybook, eslint-plugin-storybook
    - Remove storybook scripts from package.json
    - Clean up eslint config

--- .storybook/main.js
+++
@@ -1,7 +0,0 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
-  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@chromatic-com/storybook', '@storybook/addon-a11y'],
-  framework: '@storybook/react-vite',
-};
-export default config;
--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,3 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from 'eslint-plugin-storybook';
-
 import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
@@ -8,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(['dist', 'playwright-report', 'test-results', '.storybook', 'storybook-static']),
+  globalIgnores(['dist', 'playwright-report', 'test-results']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [js.configs.recommended, reactHooks.configs.flat.recommended, reactRefresh.configs.vite],
@@ -64,5 +61,4 @@ export default defineConfig([
       'react-refresh/only-export-components': 'off',
     },
   },
-  ...storybook.configs['flat/recommended'],
 ]);
